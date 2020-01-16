60c782df3ebf5ff54a5e368f6b81456c899fc9dd
Mitul Malani <mitulmalani15898@gmail.com>
2020-01-16 23:28:53 +0530

    eslint eroors solved

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import theme from './plugins/theme'
+import 'vuetify/dist/vuetify.min.css'
+import '@mdi/font/css/materialdesignicons.css'
 import axios from 'axios'
-
 // Components
 import './components'
-
-// Plugins
-import './plugins'
 
 // Sync router with store
 import { sync } from 'vuex-router-sync'
@@ -18,11 +18,14 @@ import i18n from '@/i18n'
 import router from '@/router'
 import store from '@/store'
 
-axios.defaults.baseURL = 'http://192.168.200.147:8087/'
+axios.defaults.baseURL = 'http://192.168.2.10:8087/'
 
 // Sync store with router
 sync(store, router)
-
+Vue.use(Vuetify, {
+  iconfont: 'mdi',
+  theme
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
